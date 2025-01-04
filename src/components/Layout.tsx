5bb3f96c63b5f87558cc6d91070e8cdba06a198b
ssig33 <mail@ssig33.com>
2025-01-04 10:07:31 +0900

    use darkmode only

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export interface LayoutProps {
 
 export function Layout(props: PropsWithChildren<LayoutProps>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
