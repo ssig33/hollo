3917950079025698ffa60b8823c80b2762dda96a
ssig33 <mail@ssig33.com>
2025-01-04 10:07:31 +0900

    use darkmode only

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,7 @@ export interface LayoutProps {
 export function Layout(props: PropsWithChildren<LayoutProps>) {
   const themeColor = props.themeColor ?? "azure";
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
